75935759d7c5c37dc2a3e775e1fa5b24f0c1bd49
Siigmaf <faby.tbe@gmail.com>
2023-04-24 17:00:26 -0300

    feat: Adiciona comentarios ao componente Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -14,3 +14,10 @@ function Card({ emoji, isFlipped, onClick }) {
 }
 
 export default Card;
+
+/*---------------------------------------------------------------------------------------------------------------------
+O componente Card representa um cartão com duas faces, em que a face frontal é oculta e a face traseira exibe um emoji.
+A propriedade emoji é usada para definir qual emoji será exibido na face traseira.
+A propriedade isFlipped é usada para controlar se o cartão está virado ou não.
+A propriedade onClick é usada para definir o evento que acontecerá quando o usuário clica no cartão.
+----------------------------------------------------------------------------------------------------------------------*/
